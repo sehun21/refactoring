b354785f96f54d52f7c6e0421c2a3aaafa7ccd85
sehun21 <sehun@pozalabs.com>
2021-07-14 00:12:51 +0900

    공부를 위한 초기설정

--- ch1/aki90/statement.js
+++ ch1/aki90/statement.js
@@ -1,17 +1,19 @@
 const statement = (invoice, plays) => {
-  let totalAmount = 0
-  let volumeCredits = 0
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+
   const format = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2,
-  }).format
+  }).format;
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID]
     let thisAmount = 0
 
+    // TODO 
     switch (play.type) {
       case 'tragedy': {
         thisAmount = 40000
@@ -38,4 +40,4 @@ const statement = (invoice, plays) => {
   return result
 }
 
-export default statement
+export default statement;
